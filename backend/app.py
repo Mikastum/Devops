d042179c7cbdc62a5ea7aaef65842644602346be
Mikastum <Lukashuk235@gmail.com>
2024-12-19 04:43:07 +0300

    Initial commit

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # Подключение к базе данных SQL Server
 def get_db_connection():
-    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=db;DATABASE=mydb;UID=user;PWD=password')
+    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=db;DATABASE=master;UID=sa;PWD=YourStrong!Passw0rd')
     return conn
 
 @app.route('/api', methods=['GET'])
@@ -19,9 +19,15 @@ def get_test_data():
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM test')
     rows = cursor.fetchall()
+    result = []
+    for row in rows:
+        result.append({
+            'id': row[0],
+            'name': row[1]
+        })
     cursor.close()
     conn.close()
-    return jsonify(rows)
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
